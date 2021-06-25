218125046c27c8fe3d3866f8f449bbea0ac3f185
hazuniga <74209637+hazuniga@users.noreply.github.com>
2021-06-24 22:27:29 -0500

    Construcción de proyecto

---
+++ src/main/java/com/experimentality/demo/ExperimentalityApplication.java
@@ -0,0 +1,13 @@
+package com.experimentality.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experimentality/demo/ExperimentalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experimentality.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
